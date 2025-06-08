a986ecd75f21b2ffc99d4d29e03e6435c745b0ed
maheshbvcode <vbhseh@M1947>
2025-06-08 19:15:17 +0530

    Ep2: igniting React App using parcel

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello I am H1"),
-    React.createElement("h2", {}, "Hello I am H2"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1a" }, "Hello I am H1"),
+    React.createElement("h2", { key: "h2a" }, "Hello I am H2"),
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1b" }, "Hello I am H1"),
+    React.createElement("h2", { key: "h2b" }, "Hello I am H2"),
   ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
